dfed453b7f4485c93f31b2db2bace64a6e72de40
Brinley Zhao <brinleyzhao@gmaill.com>
2022-06-29 08:41:27 -0400

    added BGM to main menu, tried to match the pace of falling object to tune (not too succesful?)

--- Assets/Scripts/Misc Scripts/Instantiator.cs
+++ Assets/Scripts/Misc Scripts/Instantiator.cs
@@ -1,33 +1,59 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Instantiator : MonoBehaviour
+namespace Misc_Scripts
 {
+  public class Instantiator : MonoBehaviour
+  {
     float timer = 0.0f;
+
+    // [SerializeField] private AudioClip foodDropSfx;
+    private int sequenceCounter = 0;
+    [SerializeField] private AudioSource BMG;
+    [SerializeField] private float longWaitTime;
+    [SerializeField] private float shortWaitTime;
+
     [SerializeField] List<GameObject> foodPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-
+      BMG.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+      // if (!BMG.gameObject.activeSelf)
+      // {
+      //   BMG.gameObject.SetActive(true);
+      // }
 
-        if (timer >= 1.0f){
-           int idx = Random.Range(0, foodPrefabs.Count);
-           var newItem = Instantiate<GameObject>(foodPrefabs[idx], transform.position, transform.rotation);
+      timer += Time.deltaTime;
 
-           if (newItem.GetComponent<AudioSource>())
-           {
-             newItem.GetComponent<AudioSource>().enabled = false;
-           }
+      if (timer >= longWaitTime)
+      {
+        int idx = Random.Range(0, foodPrefabs.Count);
+        var newItem = Instantiate<GameObject>(foodPrefabs[idx], transform.position, transform.rotation);
 
-           timer = Random.Range(0, 0.5f);
-
+        if (newItem.GetComponent<AudioSource>())
+        {
+          newItem.GetComponent<AudioSource>().enabled = false;
         }
+
+        // timer = Random.Range(0, 0.5f);
+
+        if (sequenceCounter % 4 == 0)
+        {
+          timer = 0f;
+        }
+        else
+        {
+          timer = longWaitTime - shortWaitTime;
+        }
+
+        sequenceCounter += 1;
+      }
     }
+  }
 }
